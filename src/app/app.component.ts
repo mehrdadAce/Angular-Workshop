5c10ee5662bc841b20a2cae505b12eca9fb5d6c9
mehrdadAce <mehrdad.kazemi@ac-e.be>
2022-06-07 12:13:31 +0200

    Add data.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, OnInit, VERSION } from '@angular/core';
+import { Book, Movie } from '../types';
+import { data } from './data';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent implements OnInit {
   name = 'Angular ' + VERSION.major;
+
+  items: (Book | Movie)[] = [];
+
+  ngOnInit(): void {
+    this.items = data.slice(0, 10) as (Book | Movie)[];
+  }
 }
